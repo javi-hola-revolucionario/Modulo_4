697253f0a3734a9bd6d6e07a5c3df56644108d02
Javier Martinez <javier.martinezperches01@gmail.com>
2026-03-20 20:59:34 -0600

    subiendo actividad de calcular costos de viajes y transportes

---
+++ costos.js
@@ -0,0 +1,50 @@
+const COSTO_DESTINO = {
+    Paris: 500,
+    Londres: 400,
+    "New York": 600,
+    Madrid: 350,
+    Tokyo: 700,
+};
+
+const COSTO_TRANSPORTE = {
+    Tren: 100, 
+    Autobus: 60,
+    Avion: 200,
+};
+
+const COSTO_ALOJAMIENTO_POR_NOCHE = {
+    Hotel: 90,
+    Hostal: 30,
+    Airbnb: 30,
+}
+
+// Funcion que va recibir los datos del viaje, la exportamos para usarla en otros archivos
+export const calcularCosto = ({
+    destino,
+    transporte,
+    alojamiento,
+    noches = 0,
+    personas = 1,
+}) => {
+    const baseDestino = COSTO_DESTINO[destino] ?? 0; // Si el destino no existe usamos 0 por defecto y asi con todos. 
+    const extraTransporte = COSTO_TRANSPORTE[transporte] ?? 0;
+    const costoNoche = COSTO_ALOJAMIENTO_POR_NOCHE[alojamiento] ?? 0;
+    let subtotal = ( baseDestino + extraTransporte ) * personas; // Costo del destino + el transporte (multiplicado por el numero de personas)
+    // Sumamos el costo del alojamiento 
+    // noches * precio por noche
+    subtotal += noches * costoNoche;
+
+    const descuento = 
+    // Usando operador ternario = condicion ? valor si es true : valor si es false
+    personas >= 5 ? 0.12 : // 12% de descuento
+    personas >= 3 ? 0.7 : // 7% de descuento
+    0; // sin descuento
+
+    // Aplicando el descuento al subtotal
+    const total = subtotal * ( 1 - descuento);
+
+    // Redondear el resultado
+    return Math.round(total);
+}
+
+// Me quede en minuto 46
--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-//prueba
+// Operador nullish coalescing
+let nombre = "Javi"; 
+
+// Se activa ?? cuando la variable es null/undefined
+let nombreFinal = nombre ?? "Invitado";
+console.log(nombreFinal);
+
+let edad;
+let edadFinal = edad ?? 18;
+console.log(edadFinal);
+
+// Operador AND
+let numero = 0; // Va tomar el valor como falso si utilizamos AND
+let resultado = numero || 10;
+console.log(resultado); 
