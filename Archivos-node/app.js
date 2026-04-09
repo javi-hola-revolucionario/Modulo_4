d288a2277a5d7dc47466769f5c2ac5ddafad9222
Javier Martinez <javier.martinezperches01@gmail.com>
2026-04-08 21:58:10 -0600

    Viendo la clase de node.js

--- Archivos-node/app.js
+++ Archivos-node/app.js
@@ -1,18 +1,32 @@
 // Node.js modulo llamado fs Files System, o sistema de archivos
 
-// Importando el modulo 
-const fs = required ('fs')
+// Importando el módulo fs
+const fs = require('fs');
+const path = require('path');
 
-//guardar ruta en una constante 
-const rutaArchivo = './ejemplo.txt'
+// Verificando si un archivo existe
+const filePath = path.join(__dirname, './example.txt');
 
-//verificar si un archivo existe 
-
-if(fs.existSync(rutaArchivo)){
-    //Solo si el archivo existe
-    console.log('El archivo existe');
+if (fs.existsSync(filePath)) { // Indica que nuestra funcion es sincrona
+  console.log('El archivo existe.');
 } else {
-    console.log('El archivo no existe');
+  console.log('El archivo no existe.');
 }
 
-// Me quede en minuto 20
+// Leer el contenido
+fs.readFile('.ejemplo.txt', 'utf-8', (error, contenido) => {
+
+    if(error) {
+        console.error('Ocurrio un error al leer el archivo: ', error)
+        return; // Con return detenemos esta funcion
+    }
+
+    //Si no existio ningun error
+    console.log('Contenido del archivo')
+    console.log(contenido);
+});
+
+//Escribir un archivo si no existe, o cambiar el contenido de uno existente
+fs.writeFile()
+
+// Minuto 36.45
